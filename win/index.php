7f167d4304456e709733677d092f86cac964572c
HUSSAIN ALI ALMUBARAK <hussainworkstady@gmail.com>
2025-03-11 22:23:32 +0300

    Add files via upload
    المشروع الأول المسابقة - حسين علي المبارك

---
+++ win/index.php
@@ -0,0 +1,35 @@
+<?php?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>HUSSAIN</title>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+
+<form action="index.php"></form>
+
+<input type="text" name="" id="">
+<input type="text" name="" id="">
+<input type="text" name="" id="">
+
+
+
+
+
+<script src="./js/script.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
